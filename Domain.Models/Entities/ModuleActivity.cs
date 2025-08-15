d09afdc2d569c9791fba9fbf6fa93bf184da002e
KakanDrakan <tobias.lindskog@yahoo.com>
2025-08-15 15:07:05 +0200

    Add ModuleActivity class and update database schema
    Introduces the `ModuleActivity` class with properties for managing activities. Updates the database schema to include new tables for `Courses`, `Documents`, `Submissions`, `Modules`, and `Activities`, with appropriate foreign key relationships. Modifies the `ApplicationUser` table to include a `CourseId` column, enhancing data integrity and query performance.

---
+++ Domain.Models/Entities/ModuleActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Entities;
+
+public class ModuleActivity
+{
+    public string Id { get; set; }
+    [MaxLength(30)]
+    [Required]
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public ActivityType Type { get; set; }
+    public string ModuleId { get; set; }
+
+}
+
+public enum ActivityType
+{
+    Lecture,
+    Assignment,
+    Quiz,
+    Project,
+    Discussion
+}
