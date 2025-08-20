45016036e664a78e926f4a277a5dccf909a70249
Astrid Grytberg <25738809+agrvsk@users.noreply.github.com>
2025-08-20 14:46:54 +0200

    Enhance course and member management functionality
    - Added new using directives and parameters in MemberComponent.razor.
    - Updated Course.razor with authorization and initialization logic.
    - Improved CourseList.razor to fetch courses asynchronously.
    - Introduced CallApiAsync method in ClientApiService.cs for flexible API calls.
    - Updated IApiService interface to support new API call method.
    - Added CourseService class to encapsulate course retrieval logic.
    
    These changes improve the application's structure and error handling for course-related data.

---
+++ LMS.Blazor/Services/CourseService.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using LMS.Shared.DTOs;
+using LMS.Shared.DTOs.EntityDto;
+
+namespace LMS.Blazor.Services;
+
+public class CourseService
+{
+    private readonly HttpClient httpClient;
+
+    public CourseService(IHttpClientFactory httpClientFactory)
+    {
+        httpClient = httpClientFactory.CreateClient("LmsAPIClient");
+    }
+
+    public async Task<IEnumerable<CourseDto>> GetAllCourses()
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/courses");
+        var response = await httpClient.SendAsync(request);
+
+        var content = await response.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<IEnumerable<CourseDto>>(content);
+
+        return result;
+    }
+
+
+}
