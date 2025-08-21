dc6e172a42ae20772948d384d019abe43cf35f88
PathingStar <stanislav.hallberg@hallbergfaux.se>
2025-08-21 09:57:49 +0200

    Add ScheduleService and ScheduleDto for scheduling logic
    Introduces the `ScheduleService` class to manage scheduling operations, with dependencies on `IUnitOfWork` and `ServiceManager`. A new `ScheduleDto` record is added to represent scheduling information, including start and end dates, course details, and associated modules and activities.

---
+++ LMS.Services/ScheduleService.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Contracts.Repositories;
+using LMS.Shared.DTOs.EntityDto;
+using LMS.Shared.DTOs.EntityDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Services;
+
+public class ScheduleService
+{
+    private readonly IUnitOfWork uow;
+    private readonly ServiceManager sm;
+
+    public ScheduleService(IUnitOfWork uow, ServiceManager sm)
+    {
+        this.uow = uow;
+        this.sm = sm;
+        // Initialize any required services or dependencies here
+    }
+    //public ScheduleDto GetSchedule(Guid coursId, DateTime start, DateTime end) {
+    
+    //        CourseDto course = sm.CourseService.GetCourseByIdAsync(coursId).Result;
+
+    //}
+
+}
---
+++ LMS.Shared/DTOs/EntityDTO/ScheduleDto.cs
@@ -0,0 +1,18 @@
+﻿using LMS.Shared.DTOs.EntityDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs.EntityDTO;
+
+public record ScheduleDto
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public CourseDto Course { get; set; }
+    public List<ModuleDto> Modules { get; set; } 
+
+    public List<ModuleActivityDto> ModuleActivities { get; set; } 
+}
