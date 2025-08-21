a9972f59e2747b3a469e3aedd458396cd3be3f6c
PathingStar <stanislav.hallberg@hallbergfaux.se>
2025-08-21 11:51:19 +0200

    Implement IScheduleService in ScheduleService
    Updated ScheduleService to implement IScheduleService interface, adding necessary constructor parameters for dependency injection and fixing a missing closing brace. Introduced IScheduleService interface with GetSchedule method, including required using directive for ScheduleDto.

---
+++ Service.Contracts/IScheduleService.cs
@@ -0,0 +1,8 @@
+﻿using LMS.Shared.DTOs.EntityDTO;
+
+namespace Service.Contracts;
+
+    public interface IScheduleService
+    {
+        ScheduleDto GetSchedule(Guid coursId, DateTime start, DateTime end);
+    }
