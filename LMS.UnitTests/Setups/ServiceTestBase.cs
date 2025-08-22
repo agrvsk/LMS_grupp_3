29773149368791fdbc64a332af9d423046a61409
Vilith <vilith44@gmail.com>
2025-08-22 14:01:23 +0200

    Update UserServiceTests and ServiceTestBase for testing
    Changed UserServiceTests to public and added tests for
    GetUserByIdAsync, GetAllUsersAsync, GetRolesInUser,
    GetUsersByRoleAsync, and GetUsersByCourseIdAsync methods.
    Updated ServiceTestBase to include a mock for
    UserManager<ApplicationUser> for improved user service testing.

--- LMS.UnitTests/Setups/ServiceTestBase.cs
+++ LMS.UnitTests/Setups/ServiceTestBase.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
+using Microsoft.AspNetCore.Identity;
 using Moq;
 
 namespace LMS.UnitTests.Setups
@@ -18,10 +20,12 @@ namespace LMS.UnitTests.Setups
         protected readonly Mock<IModuleRepository> MockModuleRepo;
         protected readonly Mock<IModuleActivityRepository> MockModuleActivityRepo;
         protected readonly Mock<IDocumentRepository> MockDocumentRepo;
-        
+
+        protected readonly Mock<UserManager<ApplicationUser>> MockUserManager;
 
 
-        protected ServiceTestBase() 
+
+        protected ServiceTestBase()
         {
             MockMapper = new Mock<IMapper>();
             MockUow = new Mock<IUnitOfWork>();
@@ -29,15 +33,28 @@ namespace LMS.UnitTests.Setups
             // Initialize all repository mocks
             MockModuleActivityRepo = new Mock<IModuleActivityRepository>();
             MockModuleRepo = new Mock<IModuleRepository>();
-            MockCourseRepo = new Mock<ICourseRepository>();            
+            MockCourseRepo = new Mock<ICourseRepository>();
             MockDocumentRepo = new Mock<IDocumentRepository>();
 
             // Setup the UnitOfWork to return the repository mocks
             MockUow.SetupGet(u => u.ModuleActivityRepository).Returns(MockModuleActivityRepo.Object);
             MockUow.SetupGet(u => u.ModuleRepository).Returns(MockModuleRepo.Object);
-            MockUow.SetupGet(u => u.CourseRepository).Returns(MockCourseRepo.Object);            
+            MockUow.SetupGet(u => u.CourseRepository).Returns(MockCourseRepo.Object);
             MockUow.SetupGet(u => u.DocumentRepository).Returns(MockDocumentRepo.Object);
-            
+
+            var store = new Mock<IUserStore<ApplicationUser>>();
+            MockUserManager = new Mock<UserManager<ApplicationUser>>
+                (
+                store.Object,   // IUserStore<ApplicationUser>
+                null!,          // IOptions<IdentityOptions>
+                null!,          // IPasswordHasher<ApplicationUser>
+                null!,          // IEnumerable<IUserValidator<ApplicationUser>>
+                null!,          // IEnumerable<IPasswordValidator<ApplicationUser>>
+                null!,          // ILookupNormalizer
+                null!,          // IdentityErrorDescriber
+                null!,          // IServiceProvider
+                null!           // ILogger<UserManager<ApplicationUser>>
+            );
         }
     }
 }
