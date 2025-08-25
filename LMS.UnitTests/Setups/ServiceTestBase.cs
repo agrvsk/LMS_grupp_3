2dc9bb7ae3badc1587410dbd2abcdeb0a4b2eac8
Vilith <vilith44@gmail.com>
2025-08-25 13:45:22 +0200

    Enhance AuthServiceTests and ServiceTestBase setup
    Updated AuthServiceTests to public and added new test methods for user authentication, registration, and token management.
    
    Introduced new mock repositories in ServiceTestBase for improved testing of user functionalities. Initialized JWT options for testing and added a CreateTestUser method to streamline user creation in tests.

--- LMS.UnitTests/Setups/ServiceTestBase.cs
+++ LMS.UnitTests/Setups/ServiceTestBase.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Domain.Contracts.Repositories;
+using Domain.Models.Configurations;
 using Domain.Models.Entities;
 using LMS.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 using Moq;
 using Service.Contracts;
 using System;
@@ -20,13 +22,18 @@ namespace LMS.UnitTests.Setups
         protected readonly Mock<IUnitOfWork> MockUow;
 
         // Repository Mocks
+        protected readonly Mock<IApplicationUserRepository> MockApplicationUserRepo;
         protected readonly Mock<ICourseRepository> MockCourseRepo;
+        protected readonly Mock<IDocumentRepository> MockDocumentRepo;
         protected readonly Mock<IModuleRepository> MockModuleRepo;
         protected readonly Mock<IModuleActivityRepository> MockModuleActivityRepo;
-        protected readonly Mock<IDocumentRepository> MockDocumentRepo;
+        protected readonly Mock<ISubmissionRepository> MockSubmissionRepo;
 
         // UserManager Mock
         protected readonly Mock<UserManager<ApplicationUser>> MockUserManager;
+        protected readonly Mock<RoleManager<IdentityRole>> MockRoleManager;
+
+        protected readonly IOptions<JwtSettings> JwtOptions;
 
         // Service Mocks
         protected readonly Mock<IAuthService> MockAuthService;
@@ -36,6 +43,8 @@ namespace LMS.UnitTests.Setups
         protected readonly Mock<IModuleService> MockModuleService;
         protected readonly Mock<ISubmissionService> MockSubmissionService;
         protected readonly Mock<IUserService> MockUserService;
+
+
         protected ServiceManager ServiceManager;
 
         protected ServiceTestBase()
@@ -44,19 +53,26 @@ namespace LMS.UnitTests.Setups
             MockMapper = new Mock<IMapper>();
             MockUow = new Mock<IUnitOfWork>();
 
-            /// Initialize repository mocks
-            MockModuleActivityRepo = new Mock<IModuleActivityRepository>();
-            MockModuleRepo = new Mock<IModuleRepository>();
+            // Initialize repository mocks
+            MockApplicationUserRepo = new Mock<IApplicationUserRepository>();
             MockCourseRepo = new Mock<ICourseRepository>();
             MockDocumentRepo = new Mock<IDocumentRepository>();
+            MockModuleActivityRepo = new Mock<IModuleActivityRepository>();
+            MockModuleRepo = new Mock<IModuleRepository>();
+            MockSubmissionRepo = new Mock<ISubmissionRepository>();
 
             // Setup the UnitOfWork to return the repository mocks
-            MockUow.SetupGet(u => u.ModuleActivityRepository).Returns(MockModuleActivityRepo.Object);
-            MockUow.SetupGet(u => u.ModuleRepository).Returns(MockModuleRepo.Object);
+            MockUow.SetupGet(u => u.ApplicationUserRepository).Returns(MockApplicationUserRepo.Object);
             MockUow.SetupGet(u => u.CourseRepository).Returns(MockCourseRepo.Object);
             MockUow.SetupGet(u => u.DocumentRepository).Returns(MockDocumentRepo.Object);
+            MockUow.SetupGet(u => u.ModuleActivityRepository).Returns(MockModuleActivityRepo.Object);
+            MockUow.SetupGet(u => u.ModuleRepository).Returns(MockModuleRepo.Object);
+            MockUow.SetupGet(u => u.SubmissionRepository).Returns(MockSubmissionRepo.Object);
 
-            // Initialize UserManager mock
+            // Setup UnitOfWork CompleteAsync to return a completed task
+            MockUow.Setup(u => u.CompleteAsync()).Returns(Task.CompletedTask);
+
+            // Identity Mocks
             var store = new Mock<IUserStore<ApplicationUser>>();
             MockUserManager = new Mock<UserManager<ApplicationUser>>
                 (
@@ -69,12 +85,32 @@ namespace LMS.UnitTests.Setups
                 null!,          // IdentityErrorDescriber
                 null!,          // IServiceProvider
                 null!           // ILogger<UserManager<ApplicationUser>>
-            );
+                );
+
+            var roleStore = new Mock<IRoleStore<IdentityRole>>();
+            MockRoleManager = new Mock<RoleManager<IdentityRole>>
+                (
+                roleStore.Object, // IRoleStore<IdentityRole>
+                null!,            // IEnumerable<IRoleValidator<IdentityRole>>
+                null!,            // ILookupNormalizer
+                null!,            // IdentityErrorDescriber
+                null!             // ILogger<RoleManager<IdentityRole>>
+                );
+
+            // JWT Options
+            JwtOptions = Options.Create(new JwtSettings
+            {
+                SecretKey = "SecretKeyForTestingPurposesOnly!",
+                Issuer = "TestIssuer",
+                Audience = "TestAudience",
+                Expires = 60
+            });
+
 
             // Initialize all service mocks
             MockAuthService = new Mock<IAuthService>();
             MockCourseService = new Mock<ICourseService>();
-            MockDocumentService = new Mock<IDocumentService>();            
+            MockDocumentService = new Mock<IDocumentService>();
             MockModuleActivityService = new Mock<IModuleActivityService>();
             MockModuleService = new Mock<IModuleService>();
             MockSubmissionService = new Mock<ISubmissionService>();
@@ -86,11 +122,14 @@ namespace LMS.UnitTests.Setups
                 new Lazy<ICourseService>(() => MockCourseService.Object),
                 new Lazy<IDocumentService>(() => MockDocumentService.Object),
                 new Lazy<IModuleActivityService>(() => MockModuleActivityService.Object),
-                new Lazy<IModuleService>(() => MockModuleService.Object),                
+                new Lazy<IModuleService>(() => MockModuleService.Object),
                 new Lazy<ISubmissionService>(() => MockSubmissionService.Object),
                 new Lazy<IUserService>(() => MockUserService.Object)
                 );
 
         }
+        protected ApplicationUser CreateTestUser(string username = "testuser") =>
+            new ApplicationUser { UserName = username, Id = Guid.NewGuid().ToString() };
+
     }
 }
