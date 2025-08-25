f983b6145ce24fa5012123dde351a01ee6cb5811
Vilith <vilith44@gmail.com>
2025-08-25 10:15:58 +0200

    Add course addition event handling and unit tests
    - Updated `CourseAddComponent.razor` to include an `EventCallback` for notifying the parent component when a new course is added.
    - Modified `CourseList.razor` to handle the new event and changed `Courses` from `IEnumerable` to `List` for easier management.
    - Enhanced `ServiceManagerTests.cs` by changing the class to `public` and adding unit tests for various services to ensure they return the correct instances.
    - Cleaned up `ServiceTestBase.cs` by organizing using directives, adding new service mocks, and improving comments for clarity on mock initialization.

--- LMS.UnitTests/Services/ServiceManagerTests.cs
+++ LMS.UnitTests/Services/ServiceManagerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LMS.UnitTests.Setups;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,72 @@ using System.Threading.Tasks;
 
 namespace LMS.UnitTests.Services
 {
-    internal class ServiceManagerTests
+    public class ServiceManagerTests : ServiceTestBase
     {
+
+        [Fact]
+        [Trait("ServiceManager", "AuthService")]
+        public void AuthService_ReturnsInjectedService()
+        {            
+            var result = ServiceManager.AuthService;
+
+            Assert.Same(MockAuthService.Object, result);
+        }
+
+        [Fact]
+        [Trait("ServiceManager", "CourseService")]
+        public void CourseService_ReturnsInjectedService()
+        {            
+            var result = ServiceManager.CourseService;
+
+            Assert.Same(MockCourseService.Object, result);
+        }
+
+        [Fact]
+        [Trait("ServiceManager", "DocumentService")]
+        public void DocumentService_ReturnsInjectedService()
+        {            
+            var result = ServiceManager.DocumentService;
+
+            Assert.Same(MockDocumentService.Object, result);
+        }
+
+        [Fact]
+        [Trait("ServiceManager", "ModuleActivityService")]
+        public void ModuleActivityService_ReturnsInjectedService()
+        {            
+            var result = ServiceManager.ModuleActivityService;
+
+            Assert.Same(MockModuleActivityService.Object, result);
+        }
+
+        [Fact]
+        [Trait("ServiceManager", "ModuleService")]
+        public void ModuleService_ReturnsInjectedService()
+        {            
+            var result = ServiceManager.ModuleService;
+
+            Assert.Same(MockModuleService.Object, result);
+        }
+
+        [Fact]
+        [Trait("ServiceManager", "SubmissionService")]
+        public void SubmissionService_ReturnsInjectedService()
+        {            
+            var result = ServiceManager.SubmissionService;
+
+            Assert.Same(MockSubmissionService.Object, result);
+        }
+
+        [Fact]
+        [Trait("ServiceManager", "UserService")]
+        public void UserService_ReturnsInjectedService()
+        {            
+            var result = ServiceManager.UserService;
+
+            Assert.Same(MockUserService.Object, result);
+        }
     }
+
 }
+
--- LMS.UnitTests/Setups/ServiceTestBase.cs
+++ LMS.UnitTests/Setups/ServiceTestBase.cs
@@ -1,36 +1,50 @@
-﻿using System;
+﻿using AutoMapper;
+using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
+using LMS.Services;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using Service.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
-using Domain.Contracts.Repositories;
-using Domain.Models.Entities;
-using Microsoft.AspNetCore.Identity;
-using Moq;
 
 namespace LMS.UnitTests.Setups
 {
     public abstract class ServiceTestBase
     {
+        // Common Mocks
         protected readonly Mock<IMapper> MockMapper;
         protected readonly Mock<IUnitOfWork> MockUow;
 
+        // Repository Mocks
         protected readonly Mock<ICourseRepository> MockCourseRepo;
         protected readonly Mock<IModuleRepository> MockModuleRepo;
         protected readonly Mock<IModuleActivityRepository> MockModuleActivityRepo;
         protected readonly Mock<IDocumentRepository> MockDocumentRepo;
 
+        // UserManager Mock
         protected readonly Mock<UserManager<ApplicationUser>> MockUserManager;
 
-
+        // Service Mocks
+        protected readonly Mock<IAuthService> MockAuthService;
+        protected readonly Mock<ICourseService> MockCourseService;
+        protected readonly Mock<IDocumentService> MockDocumentService;
+        protected readonly Mock<IModuleActivityService> MockModuleActivityService;
+        protected readonly Mock<IModuleService> MockModuleService;
+        protected readonly Mock<ISubmissionService> MockSubmissionService;
+        protected readonly Mock<IUserService> MockUserService;
+        protected ServiceManager ServiceManager;
 
         protected ServiceTestBase()
         {
+            // Initialize common mocks
             MockMapper = new Mock<IMapper>();
             MockUow = new Mock<IUnitOfWork>();
 
-            // Initialize all repository mocks
+            /// Initialize repository mocks
             MockModuleActivityRepo = new Mock<IModuleActivityRepository>();
             MockModuleRepo = new Mock<IModuleRepository>();
             MockCourseRepo = new Mock<ICourseRepository>();
@@ -42,6 +56,7 @@ namespace LMS.UnitTests.Setups
             MockUow.SetupGet(u => u.CourseRepository).Returns(MockCourseRepo.Object);
             MockUow.SetupGet(u => u.DocumentRepository).Returns(MockDocumentRepo.Object);
 
+            // Initialize UserManager mock
             var store = new Mock<IUserStore<ApplicationUser>>();
             MockUserManager = new Mock<UserManager<ApplicationUser>>
                 (
@@ -55,6 +70,27 @@ namespace LMS.UnitTests.Setups
                 null!,          // IServiceProvider
                 null!           // ILogger<UserManager<ApplicationUser>>
             );
+
+            // Initialize all service mocks
+            MockAuthService = new Mock<IAuthService>();
+            MockCourseService = new Mock<ICourseService>();
+            MockDocumentService = new Mock<IDocumentService>();            
+            MockModuleActivityService = new Mock<IModuleActivityService>();
+            MockModuleService = new Mock<IModuleService>();
+            MockSubmissionService = new Mock<ISubmissionService>();
+            MockUserService = new Mock<IUserService>();
+
+            // Initialize the ServiceManager with the service mocks
+            ServiceManager = new ServiceManager(
+                new Lazy<IAuthService>(() => MockAuthService.Object),
+                new Lazy<ICourseService>(() => MockCourseService.Object),
+                new Lazy<IDocumentService>(() => MockDocumentService.Object),
+                new Lazy<IModuleActivityService>(() => MockModuleActivityService.Object),
+                new Lazy<IModuleService>(() => MockModuleService.Object),                
+                new Lazy<ISubmissionService>(() => MockSubmissionService.Object),
+                new Lazy<IUserService>(() => MockUserService.Object)
+                );
+
         }
     }
 }
