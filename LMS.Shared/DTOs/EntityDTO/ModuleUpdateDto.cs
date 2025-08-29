f9deaa52a161d9c7d1d277d06c1f09c0bee92b49
KakanDrakan <tobias.lindskog@yahoo.com>
2025-08-29 12:07:20 +0200

    moduleUpdateDto Description is now nullable

--- LMS.Shared/DTOs/EntityDTO/ModuleUpdateDto.cs
+++ LMS.Shared/DTOs/EntityDTO/ModuleUpdateDto.cs
@@ -14,7 +14,7 @@ public record ModuleUpdateDto
     [MaxLength(100)]
     [Required]
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public Guid CourseId { get; set; }
