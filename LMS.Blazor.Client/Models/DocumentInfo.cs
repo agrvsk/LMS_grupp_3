453e9f1a53ca903a9ab864de53aa11cc73ce3376
Vilith <vilith44@gmail.com>
2025-09-03 15:23:26 +0200

    Fixed with memorystream in ProxyController.cs
    Fixed DocumentEditComponent
    While implementing it. Should be able to change name and/or Description on uploaded items.
    
    Also some tweaks for it to work in DocumentsController. DocumentService
    
    And added a DocumentEditDto

--- LMS.Blazor.Client/Models/DocumentInfo.cs
+++ LMS.Blazor.Client/Models/DocumentInfo.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
 
 namespace LMS.Blazor.Client.Models
 {
     public record DocumentInfo
     {
-        
+        [Required(ErrorMessage = "Document name is required")]
+        [StringLength(100, ErrorMessage = "Name is too long (max 100 characters)")]
+        public string? Name  { get; set; }
         public IBrowserFile? File { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description is too long (max 500 characters)")]
         public string? Description { get; set; }
     }
 }
