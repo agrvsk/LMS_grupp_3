9a67b5cb2f981b9fc279ed2c81b425ac92927a55
Astrid Grytberg <25738809+agrvsk@users.noreply.github.com>
2025-10-03 16:33:07 +0200

    TEST

--- User.sql
+++ User.sql
@@ -1,5 +1,10 @@
-﻿CREATE USER "astrix-api" FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER "astrix-api";
-ALTER ROLE db_datawriter ADD MEMBER "astrix-api";	
-ALTER ROLE db_ddladmin ADD MEMBER "astrix-api";
-
+﻿/*
+CREATE USER astrixApi FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER astrixApi;
+ALTER ROLE db_datawriter ADD MEMBER astrixApi;	
+ALTER ROLE db_ddladmin ADD MEMBER astrixApi;
+*/
+CREATE USER astrixLms FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER astrixLms;
+ALTER ROLE db_datawriter ADD MEMBER astrixLms;	
+ALTER ROLE db_ddladmin ADD MEMBER astrixLms;
